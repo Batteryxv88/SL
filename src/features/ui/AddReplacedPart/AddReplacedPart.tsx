598a61428bd574952dce71dfee57169738ffa563
Max Kovalev <batteryxv88@yandex.ru>
2024-01-11 21:02:33 +0300

    add form validation

--- src/features/ui/AddReplacedPart/AddReplacedPart.tsx
+++ src/features/ui/AddReplacedPart/AddReplacedPart.tsx
@@ -13,6 +13,7 @@ import { partsFilter } from "../../lib/partsFilter/partsFilter";
 import { findIdByPartNAndLatestDate } from "../../lib/findIdByPartNandDate/findByNumberAndDate";
 import { doc, updateDoc } from "firebase/firestore";
 import db from "../../../app/config/fbConfig";
+import { useForm } from "react-hook-form";
 
 const AddReplacedPart = () => {
     const stockData: any = useSelector<any>((state) => state.parts.partsArray);
@@ -23,7 +24,7 @@ const AddReplacedPart = () => {
     const dispatch = useDispatch<any>();
 
     const [partN, setPartN] = useState<string>("");
-    const [serviceLife, setServiceLife] = useState<string>("");
+    const [serviceLife, setServiceLife] = useState<number>();
     const [quantity, setQuantity] = useState<number>(0);
     const [date, setDate] = useState<any>("");
     const [man, setMan] = useState<string>("");
@@ -40,6 +41,22 @@ const AddReplacedPart = () => {
             });
     }, [man, quantity]);
 
+    type FormValues = {
+        partN: string;
+        serviceLife: number;
+        quantity: number;
+        man: string;
+    };
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        reset,
+    } = useForm<FormValues>({
+        mode: "onChange",
+    });
+
     const sameColorParts = [
         "A5WH0Y0C",
         "A5WH0Y0M",
@@ -55,27 +72,78 @@ const AddReplacedPart = () => {
         "A50UR70244K",
     ];
 
-    const handleAddPart = (e: React.FormEvent) => {
-        e.preventDefault();
 
-        const currentPart = stockData.filter((item: any) => {
-            if (sameColorParts.includes(partN)) {
-                return item.part.partN === partN.slice(0, -1);
+    const handleAddPart = (e: any) => {
+
+        // const currentPart = stockData.filter((item: any) => {
+        //     if (sameColorParts.includes(partN)) {
+        //         return item.part.partN === partN.slice(0, -1);
+        //     }
+        //     return item.part.partN === partN;
+        // });
+
+        // const newQuantity = currentPart[0].part.quantity - quantity;
+        // const newName = currentPart[0].part.partName;
+        // const section = partsFilter(partN);
+        // const newServiceLife = serviceLife;
+
+        // const partU = {
+        //     partN: partN,
+        //     quantity: quantity,
+        //     date: date,
+        //     section: section,
+        //     man: man,
+        //     partName: newName,
+        // };
+
+        // const updatedStockPart = {
+        //     id: currentPart[0].id,
+        //     part: {
+        //         quantity: newQuantity,
+        //     },
+        // };
+
+        // const idForUpdate = findIdByPartNAndLatestDate(
+        //     replacedPartsdata,
+        //     partN
+        // );
+
+        // const updatedPart = {
+        //     id: idForUpdate,
+        //     part: {
+        //         serviceLife: newServiceLife,
+        //     },
+        // };
+
+        // dispatch(updateUsedPart(updatedPart));
+        // dispatch(updateStock(updatedStockPart));
+        // dispatch(addUsedPart(partU));
+
+        // setPartN("");
+        // setServiceLife("");
+        // setQuantity(0);
+        // setMan("");
+
+
+
+         const currentPart = stockData.filter((item: any) => {
+            if (sameColorParts.includes(e.partN)) {
+                return item.part.partN === e.partN.slice(0, -1);
             }
-            return item.part.partN === partN;
+            return item.part.partN === e.partN;
         });
 
-        const newQuantity = currentPart[0].part.quantity - quantity;
+        const newQuantity = currentPart[0].part.quantity - Number(e.quantity);
         const newName = currentPart[0].part.partName;
-        const section = partsFilter(partN);
+        const section = partsFilter(e.partN);
         const newServiceLife = serviceLife;
 
         const partU = {
-            partN: partN,
-            quantity: quantity,
+            partN: e.partN,
+            quantity: Number(e.quantity),
             date: date,
             section: section,
-            man: man,
+            man: e.man,
             partName: newName,
         };
 
@@ -88,72 +156,109 @@ const AddReplacedPart = () => {
 
         const idForUpdate = findIdByPartNAndLatestDate(
             replacedPartsdata,
-            partN
+            e.partN
         );
 
         const updatedPart = {
             id: idForUpdate,
             part: {
-                serviceLife: newServiceLife,
+                serviceLife: e.serviceLife,
             },
         };
-
 
         dispatch(updateUsedPart(updatedPart));
         dispatch(updateStock(updatedStockPart));
         dispatch(addUsedPart(partU));
 
-        setPartN("");
-        setServiceLife("");
-        setQuantity(0);
-        setPartName("");
-        setMan("");
+        reset();
+
+        // setPartN("");
+        // setServiceLife(0);
+        // setQuantity(0);
+        // setMan("");
     };
 
     return (
-        <form className={cls.addReplacedPart} onSubmit={handleAddPart}>
-            <div className={cls.box}>
-                <label className={cls.label}>Парт номер</label>
-                <input
-                    className={cls.input}
-                    onChange={(e) => setPartN(e.target.value.trim())}
-                    value={partN}
-                ></input>
+        <>
+            <form
+                className={cls.addReplacedPart}
+                onSubmit={handleSubmit(handleAddPart)}
+            >
+                <div className={cls.box}>
+                    <label className={cls.label}>Парт номер</label>
+                    <input
+                        {...register("partN", {
+                            required: "Обязательное поле",
+                            minLength: {
+                                value: 5,
+                                message: "Минимум 5 символов",
+                            },
+                        })}
+                        className={cls.input}
+                        // onChange={(e) => setPartN(e.target.value.trim())}
+                        // value={partN}
+                    ></input>
+                </div>
+                <div className={cls.box}>
+                    <label className={cls.label}>Пройденный ресурс</label>
+                    <input
+                        {...register("serviceLife", {
+                            required: "Обязательное поле",
+                            minLength: {
+                                value: 3,
+                                message: "Минимум 3 символа",
+                            },
+                        })}
+                        className={cls.input}
+                        // onChange={(e) => setServiceLife(e.target.value)}
+                        // value={serviceLife}
+                        type="number"
+                    ></input>
+                </div>
+                <div className={cls.box}>
+                    <label className={cls.label}>Кол-во</label>
+                    <input
+                        {...register("quantity", {
+                            required: "Обязательное поле",
+                            minLength: {
+                                value: 1,
+                                message: "Минимум 1 символ",
+                            },
+                        })}
+                        className={cls.inputQty}
+                        type="number"
+                        // onChange={(e: any) => setQuantity(e.target.value)}
+                        // value={quantity}
+                    ></input>
+                </div>
+                <div className={cls.box}>
+                    <label className={cls.label}>Ответственный</label>
+                    <select
+                        {...register("man", {
+                            required: "Обязательное поле",
+                        })}
+                        className={cls.input}
+                        // onChange={(e: any) => setMan(e.target.value)}
+                        // value={man}
+                    >
+                        <option value={"Алексей"}>Алексей</option>
+                        <option value={"Максим"}>Максим</option>
+                        <option value={"Сергей"}>Сергей</option>
+                    </select>
+                </div>
+                <button className={cls.button} type="submit">
+                    Добавить
+                </button>
+            </form>
+            <div className={cls.errMessage}>
+                {(errors?.partN && <p>{errors?.partN.message}</p>) ||
+                    (errors?.man && <p>{errors?.man.message}</p>) ||
+                    (errors?.quantity && <p>{errors?.quantity.message}</p>) ||
+                    (errors?.serviceLife && (
+                        <p>{errors?.serviceLife.message}</p>
+                    ))}
             </div>
-            <div className={cls.box}>
-                <label className={cls.label}>Пройденный ресурс</label>
-                <input
-                    className={cls.input}
-                    onChange={(e) => setServiceLife(e.target.value)}
-                    value={serviceLife}
-                ></input>
-            </div>
-            <div className={cls.box}>
-                <label className={cls.label}>Кол-во</label>
-                <input
-                    className={cls.inputQty}
-                    type="number"
-                    onChange={(e: any) => setQuantity(e.target.value)}
-                    value={quantity}
-                ></input>
-            </div>
-            <div className={cls.box}>
-                <label className={cls.label}>Ответственный</label>
-                <select
-                    className={cls.input}
-                    onChange={(e: any) => setMan(e.target.value)}
-                    value={man}
-                >
-                    <option value={""}></option>
-                    <option value={"Алексей"}>Алексей</option>
-                    <option value={"Максим"}>Максим</option>
-                    <option value={"Сергей"}>Сергей</option>
-                </select>
-            </div>
-            <button className={cls.button} type="submit">
-                Добавить
-            </button>
-        </form>
+        </>
     );
 };
 
--- src/features/ui/AddToner/AddToner.tsx
+++ src/features/ui/AddToner/AddToner.tsx
@@ -2,74 +2,109 @@ import { useState, useEffect } from "react";
 import cls from "./AddToner.module.scss";
 import { useDispatch } from "react-redux";
 import { addToner } from "../../../app/providers/StoreProvider/Store/TonerSlice";
+import { useForm } from "react-hook-form";
 
 const AddToner = () => {
-const [color, setColor] = useState('')
-const [man, setMan] = useState('')
-const [counter, setCounter] = useState('')
-const [date, setDate] = useState<any>('');
+    const [date, setDate] = useState<any>("");
 
-const dispatch = useDispatch<any>()
+    const dispatch = useDispatch<any>();
 
-useEffect(() => {
-    fetch("https://worldtimeapi.org/api/timezone/Europe/Moscow" )
-        .then((res) => {
-            return res.json();
-        })
-        .then((date) => setDate(date.datetime))
-        .catch((err) => {
-            console.log("Ошибка. Запрос не выполнен: ", err);
-        });
-}, [dispatch]);
+    useEffect(() => {
+        fetch("https://worldtimeapi.org/api/timezone/Europe/Moscow")
+            .then((res) => {
+                return res.json();
+            })
+            .then((date) => setDate(date.datetime))
+            .catch((err) => {
+                console.log("Ошибка. Запрос не выполнен: ", err);
+            });
+    }, [dispatch]);
 
-const handleAddToner = (e: any) => {
-    e.preventDefault()
-    const newToner = {
-        color: color,
-        man: man,
-        counter: counter,
-        date: date
-    }
+    type FormValues = {
+        color: string;
+        man: string;
+        counter: number;
+    };
 
-    dispatch(addToner(newToner))
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        reset,
+    } = useForm<FormValues>({
+        mode: "onChange",
+    });
 
-    setColor('')
-    setMan('')
-    setCounter('')
-}
+    const handleAddToner = (e: any) => {
+        const newToner = {
+            color: e.color,
+            man: e.man,
+            counter: Number(e.counter),
+            date: date,
+        };
 
+        dispatch(addToner(newToner));
+
+        reset();
+    };
 
     return (
-        <form className={cls.addToner} onSubmit={handleAddToner}>
-            <div className={cls.box}>
-                <label>Цвет</label>
-                <select value={color} onChange={(e: any) => setColor(e.target.value)} className={cls.input}>
-                    <option value={""}></option>
-                    <option value={"C"}>C</option>
-                    <option value={"M"}>M</option>
-                    <option value={"Y"}>Y</option>
-                    <option value={"K"}>K</option>
-                </select>
+        <>
+            <form
+                className={cls.addToner}
+                onSubmit={handleSubmit(handleAddToner)}
+            >
+                <div className={cls.box}>
+                    <label>Цвет</label>
+                    <select
+                        {...register("color", {
+                            required: "Обязательное поле",
+                        })}
+                        className={cls.input}
+                    >
+                        <option value={"C"}>C</option>
+                        <option value={"M"}>M</option>
+                        <option value={"Y"}>Y</option>
+                        <option value={"K"}>K</option>
+                    </select>
+                </div>
+                <div className={cls.box}>
+                    <label className={cls.label}>Ответственный</label>
+                    <select
+                        {...register("man", {
+                            required: "Обязательное поле",
+                        })}
+                        className={cls.input}
+                    >
+                        <option value={"Алексей"}>Алексей</option>
+                        <option value={"Максим"}>Максим</option>
+                        <option value={"Сергей"}>Сергей</option>
+                    </select>
+                </div>
+                <div className={cls.box}>
+                    <label className={cls.label}>Счетчик</label>
+                    <input
+                        {...register("counter", {
+                            required: "Обязательное поле",
+                            minLength: {
+                                value: 5,
+                                message: "Минимум 5 символов",
+                            },
+                        })}
+                        className={!errors.counter ? cls.input : cls.error}
+                        type="number"
+                    ></input>
+                </div>
+                <button className={cls.button} type="submit">
+                    Добавить
+                </button>
+            </form>
+            <div className={cls.errMessage}>
+                {(errors?.counter && <p>{errors?.counter.message}</p>) ||
+                    (errors?.man && <p>{errors?.man.message}</p>) ||
+                    (errors?.color && <p>{errors?.color.message}</p>)}
             </div>
-
-            <div className={cls.box}>
-                <label className={cls.label}>Ответственный</label>
-                <select value={man} onChange={(e: any) => setMan(e.target.value)} className={cls.input}>
-                    <option value={""}></option>
-                    <option value={"Алексей"}>Алексей</option>
-                    <option value={"Максим"}>Максим</option>
-                    <option value={"Сергей"}>Сергей</option>
-                </select>
-            </div>
-
-            <div className={cls.box}>
-                <label className={cls.label}>Счетчик</label>
-                <input value={counter} onChange={(e: any) => setCounter(e.target.value)} className={cls.input} type="number"></input>
-            </div>
-            <button className={cls.button} type="submit">
-                Добавить
-            </button>
-        </form>
+        </>
     );
 };
 
