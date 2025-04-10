88a10e384336803f8e0feab8e84fb495004b7320
Heidelberg Service <batteryxv88@yandex.ru>
2025-04-10 18:45:03 +0300

    added image to PaperStockPage

--- src/pages/PaperStockPage/ui/PaperStockPage.tsx
+++ src/pages/PaperStockPage/ui/PaperStockPage.tsx
@@ -1,4 +1,6 @@
 import cls from "./PaperStockPage.module.scss";
+import PaperRollImage from "../../../shared/ui/PaperRollImage/PaperRollImage";
+
 
 const PaperStockPage = () => {
     return (
@@ -8,36 +10,43 @@ const PaperStockPage = () => {
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>FA</h3>
                     <h4 className={cls.paperBox__subtitle}>Пленка акрил</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>100</data>
                 </div>
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>FH</h3>     
                     <h4 className={cls.paperBox__subtitle}>Пленка каучук</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>50</data>
                 </div>
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>PA</h3>
                     <h4 className={cls.paperBox__subtitle}>Бумага акрил</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>45</data>
                 </div>
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>PH</h3>
                     <h4 className={cls.paperBox__subtitle}>Бумага каучук</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>14</data>
                 </div>
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>Clear</h3>
                     <h4 className={cls.paperBox__subtitle}>Пленка прозрачная</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>44</data>
                 </div>
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>Metall</h3>
                     <h4 className={cls.paperBox__subtitle}>Пленка металлизированная</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>13</data>
                 </div>
                 <div className={cls.paperBox}>
                     <h3 className={cls.paperBox__title}>Verge</h3>
                     <h4 className={cls.paperBox__subtitle}>Бумага тиснёная</h4>
+                    <PaperRollImage />
                     <data className={cls.paperBox__data}>89</data>
                 </div>
             </div>
